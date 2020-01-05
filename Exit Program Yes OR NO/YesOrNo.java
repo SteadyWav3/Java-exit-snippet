bfee15faac87536ad1b1d84d6968b75ec06dba87
Faruq Raji <lovelyadeola@gmail.com>
2020-01-06 00:54:03 +0100

    Add files via upload

---
+++ Exit Program Yes OR NO/YesOrNo.java
@@ -0,0 +1,15 @@
+// Place inside the main Method!!!
+
+char ch;
+
+do {		
+	System.out.println();
+    System.out.print("\nDo you want to continue (Type y or n) : ");
+	ch = input.next().CharAt(0);
+} while(ch == "Y" || ch == "y");
+System.out.println();
+System.out.print("\t THANK YOU FOR USING THIS PROGRAM");
+System.out.println("\n\n");
+
+
+
